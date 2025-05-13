ae9b08c09a82fde1e3a40010c8e9dd1a3088e3f0
Elfar Alfreðsson <elfar@elfar.is>
2025-05-13 09:52:00 +0000

    re-enable 404

--- src/FastDatastar/Features/Site/Endpoints/Get404Page.cs
+++ src/FastDatastar/Features/Site/Endpoints/Get404Page.cs
@@ -1,23 +1,23 @@
-// using FastEndpoints;
-// using Microsoft.AspNetCore.Http;
-// using Microsoft.AspNetCore.Mvc;
-// using StarFederation.Datastar.DependencyInjection;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StarFederation.Datastar.DependencyInjection;
 
-// namespace FastDatastar.Features.Site.Endpoints;
+namespace FastDatastar.Features.Site.Endpoints;
 
-// public class Get404Page : EndpointWithoutRequest
-// {
-//     public override void Configure()
-//     {
-//         Verbs(Http.GET);
-//         Routes("{*path:regex(^((?!\\.).)*$)}");
-//         AllowAnonymous();                // no auth required
-//         // Summary(s => s.WithTags("Errors"));
-//     }
+public class Get404Page : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Verbs(Http.GET);
+        Routes("{*path:regex(^((?!\\.).)*$)}");
+        AllowAnonymous();                // no auth required
+        // Summary(s => s.WithTags("Errors"));
+    }
 
-//     public override async Task HandleAsync(CancellationToken ct)
-//     {
-//         var path = Route<string>("path");
-//         await Results.Extensions.View("Errors/404", new { Path = path }).ExecuteAsync(this.HttpContext);
-//     }
-// }
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var path = Route<string>("path");
+        await Results.Extensions.View("Errors/404", new { Path = path }).ExecuteAsync(this.HttpContext);
+    }
+}
